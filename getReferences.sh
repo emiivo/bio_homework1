81761ba1ec8f4208a9ca9d6fd5fa1c699b3c47d2
emiivo <100207998+emiivo@users.noreply.github.com>
2023-03-22 19:33:32 +0200

    Update getReferences.sh

--- getReferences.sh
+++ getReferences.sh
@@ -3,13 +3,13 @@
 #Download data of Mus Musculus version mm9
 
 #Download the reference genome in FASTA format:
-wget https://hgdownload.soe.ucsc.edu/goldenPath/mm9/bigZips/mm9.fa.gz -P /home/bioinformatikai/HW1/references/
+wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M1/NCBIM37.genome.fa.gz -O /home/bioinformatikai/HW1/references/mm9.fa.gz
 
 #Download the reference transcriptome in FASTA format:
-wget https://hgdownload.soe.ucsc.edu/goldenPath/mm9/bigZips/refMrna.fa.gz -O /home/bioinformatikai/HW1/references/mm9_RNA.fa.gz
+wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M1/gencode.vM1.pc_transcripts.fa.gz -O /home/bioinformatikai/HW1/references/mm9_RNA.fa.gz
 
 #Download GTF/GFF3 file for your reference genome:
-wget https://hgdownload.soe.ucsc.edu/goldenPath/mm9/bigZips/genes/mm9.refGene.gtf.gz -P /home/bioinformatikai/HW1/references/
+wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M1/gencode.vM1.annotation.gtf.gz -O /home/bioinformatikai/HW1/references/mm9.gtf.gz
 
 #Download raw FASTQ files:
 prefetch -O /home/bioinformatikai/HW1/inputs SRR8985047 SRR8985048 SRR8985051 SRR8985052
