cddc4c1622e89bfbc5bf465bf85835930bf9afd4
emiivo <100207998+emiivo@users.noreply.github.com>
2023-03-24 19:06:51 +0200

    Update mapping_free_preproc.sh

--- mapping_free_preproc.sh
+++ mapping_free_preproc.sh
@@ -3,8 +3,8 @@
 REFERENCE_DIR="/home/bioinformatikai/HW1/references"
 TRANSCRIPTOME_FILE="${REFERENCE_DIR}/mm9_RNA.fa.gz"
 TRANSCRIPTOME_INDEX="${REFERENCE_DIR}/mm9_RNA_index"
-INPUT_DIR=/home/bioinformatikai/HW1/inputs
-OUTPUT_DIR=/home/bioinformatikai/HW1/outputs
+INPUT_DIR="/home/bioinformatikai/HW1/inputs"
+OUTPUT_DIR="/home/bioinformatikai/HW1/outputs"
 FILES=(SRR8985047 SRR8985048 SRR8985051 SRR8985052)
 
 # CHeck if index exists for transcriptome, and if it does not, create one.
